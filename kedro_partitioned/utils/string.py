4da9afcd20ff7b85bf718698e8c8c742258a5df9
nickolasrm <nickolasrochamachado@gmail.com>
2024-06-18 03:51:09 -0300

    fix: multipipeline not considering previous nodes

--- kedro_partitioned/utils/string.py
+++ kedro_partitioned/utils/string.py
@@ -1,55 +1,6 @@
 """Utils for string manipulation."""
 
-from pathlib import Path, PosixPath, PurePath, PurePosixPath, WindowsPath
-from typing import Any
-from upath import UPath as _UPath
-
-
-class UPath(_UPath):
-    """An universal Path that doesn't return WindowsPaths.
-
-    if a Windows was returned by UPath, it converts to a PurePosixPath
-    because Kedro always use PosixPath by default.
-
-    Args:
-        *args (str): Paths to be parsed and joined (if multiple).
-
-    Example:
-        >>> UPath('C:/Users/user/Desktop/file.txt')
-        PurePosixPath('C:/Users/user/Desktop/file.txt')
-
-        >>> UPath('http://azure.com/file.txt')
-        HTTPPath('http://azure.com/file.txt')
-
-        >>> UPath('a/b/c', 'd')
-        PurePosixPath('a/b/c/d')
-
-        >>> UPath('s3://test_bucket/file.txt')
-        S3Path('s3://test_bucket/file.txt')
-    """
-
-    def __new__(cls, path: str, *args: Any, **kwargs: Any) -> PurePath:
-        """Create a new UPath object.
-
-        Args:
-            path (str): The path to be converted.
-
-        Returns:
-            PurePath: The converted path.
-        """
-        path = super().__new__(cls, path, *args, **kwargs)
-        if isinstance(path, WindowsPath):
-            if path.is_absolute():
-                parts = path.parts
-                drive, parts = parts[0], (parts[1:] if len(parts) > 1 else [])
-                drive = drive.strip("\\")
-                return PurePosixPath(drive, *parts)
-            else:
-                return PurePosixPath(path)
-        elif isinstance(path, PosixPath):
-            return PurePosixPath(path)
-        else:
-            return path
+from pathlib import Path
 
 
 def get_filepath_extension(filepath: str) -> str:
